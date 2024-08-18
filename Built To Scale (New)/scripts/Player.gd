4d9d6629e598bccc3562539e9c8a6ee2ebf3d8c3
Wolcean887 <mia.vandaelen@outlook.fr>
2024-08-18 23:05:50 +0200

    Add files via upload

--- Built To Scale (New)/scripts/Player.gd
+++ Built To Scale (New)/scripts/Player.gd
@@ -1,35 +1,31 @@
 extends CharacterBody2D
 
-@export var SPEED = 600.0 #I liked 400
-@export var JUMP_VELOCITY = -1200.0 #I liked -800
-@export var DASH_VELOCITY = 2000
+@export var SPEED = 600.0 #Player speed.
+@export var JUMP_VELOCITY = -1200.0 #Player jump power.
+@export var DASH_VELOCITY = 2000 #Player dash power.
 
-var can_dash = true
-var default_respawn = true
-var respawn_point : Vector2
+var can_dash = true #Variable which checks if the player is able to dash.
+var default_respawn = true #Variable which checks if the player has gone through a checkpoint or not.
+var respawn_point : Vector2 #Variable with some coordinates for the player to respawn at.
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-@export var gravity = 2400 #I liked 2400 (needs to be bigger to account for * delta below)
+@export var GRAVITY = 2400
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
-		#multiplying by delta means speed won't slow if framerate slows
-		velocity.y += gravity * delta
+		velocity.y += GRAVITY * delta #Makes the player fall when not on the ground based on gravity.
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
+	
+	# Handle dash.
 	if Input.is_action_pressed("dash"):
 		if can_dash:
 			velocity.x = direction * DASH_VELOCITY
@@ -38,19 +34,27 @@ func _physics_process(delta):
 			
 	move_and_slide()
 
-func _on_dash_timer_timeout():
-	can_dash = false
+func _on_dash_timer_timeout(): #Timer which allows the player to dash only for a short amount of time.
+	can_dash = false #Once the timer has reached 0, the player cannot dash anymore.
 	
-func _on_can_dash_timer_timeout():
-	can_dash = true
+func _on_can_dash_timer_timeout(): #Timer which allows the player to dash again after a short amoutn of time has passed.
+	can_dash = true #Once the timer has reached 0, the player can dash again.
 	
-func _on_checkpoint_new_checkpoint():
-	default_respawn = false
-	respawn_point = position
+func _on_checkpoint_new_checkpoint(): #Function that runs when the player touches a checkpoint that hasn't been activated before.
+	default_respawn = false 
+	respawn_point = position #The player's respawn location is changed from the default one to the checkpoint's location.
 	
-func _on_projectile_body_entered(_body):
-	if default_respawn:
+func _on_hurt_detection_body_entered(_body): #Function that runs when the player touches a spike.
+	_death()
+
+func _on_hurt_detection_area_entered(area): #Function that runs when the player touches a projectile.
+	if area.is_in_group("Projectiles"):
+		_death()
+	
+func _death(): #Function that runs when the player dies.
+	velocity.y = 0
+	if default_respawn: #If the player hasn't activated any checkpoints, they respawn to the default respawn location.
 		position.x = -1725
-		position.y = 320
-	else:
+		position.y = 336
+	else: #If the player has activated a checkpoint, they respawn to the checkpoint's location.
 		position = respawn_point
--- Built To Scale (New)/scripts/projectile.gd
+++ Built To Scale (New)/scripts/projectile.gd
@@ -1,9 +1,13 @@
 extends Area2D
 
-@export var PROJECTILE_SPEED = 200
+@export var PROJECTILE_SPEED = 200 #Projectile speed.
+signal projectile_player_contact
 
 func _process(delta):
 	position += transform.x * PROJECTILE_SPEED * delta
-	
-func _on_body_entered(_body):
+
+func _on_body_entered(_body): #When the projectile hits the player or a platform, it gets deleted.
+	destroy()
+
+func destroy():
 	queue_free()

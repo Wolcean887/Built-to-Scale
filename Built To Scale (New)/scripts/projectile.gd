b5ac70e9081cb81f04490a60a6f50e985b152dd5
Wolcean887 <mia.vandaelen@outlook.fr>
2024-08-18 19:01:28 +0200

    Add files via upload

--- Built To Scale (New)/scripts/projectile.gd
+++ Built To Scale (New)/scripts/projectile.gd
@@ -1,9 +1,9 @@
 extends Area2D
 
-@export var PROJECTILE_SPEED = 200
+@export var PROJECTILE_SPEED = 200 #Projectile speed.
 
 func _process(delta):
 	position += transform.x * PROJECTILE_SPEED * delta
 	
-func _on_body_entered(_body):
+func _on_body_entered(_body): #When the projectile hits the player, it gets deleted.
 	queue_free()

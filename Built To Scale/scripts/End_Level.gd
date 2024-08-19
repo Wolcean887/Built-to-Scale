25a6e14b08861898adc25e8a266f5b3254ee271b
Wolcean887 <mia.vandaelen@outlook.fr>
2024-08-19 19:51:59 +0200

    Delete Built To Scale directory

--- Built To Scale/scripts/End_Level.gd
+++
@@ -1,5 +0,0 @@
-extends Area2D
-
-
-func _on_area_entered(_area):
-	print ("You reached the end! :D")
--- Built To Scale/scripts/turret.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-var projectile_scene : PackedScene = preload("res://scenes/projectile.tscn")
-
-func _on_turret_shoot_timeout():
-	var projectile = projectile_scene.instantiate()
-	projectile.position.x += 60 
-	add_child(projectile)
-
